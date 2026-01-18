a97d9184f277d8f8dbc61ea0c5d402fbc5858afe
Greehara <astradoesthis@gmail.com>
2026-01-19 08:58:52 +1100

    Prototype 2
    Added player animations, tweaked player script, edited tileset and created two others, created new test map

--- the-castle/Player/player.gd
+++ the-castle/Player/player.gd
@@ -1,16 +1,29 @@
 extends CharacterBody2D
 
-const max_speed: int = 80
-const acceleration: int = 5
-const friction: int = 8
+const max_speed: float = 80
+const acceleration: float = 5
+const friction: float = 16
+
+@export var current_max_speed: float = max_speed
 
 func _physics_process(delta: float) -> void:
-	var Input = Vector2(
-		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
-		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	).normalized()
-	
-	var lerp_weight = delta * (acceleration if Input else friction)
-	velocity = lerp(velocity, Input * max_speed, lerp_weight)
+	movement(delta)
 	
 	move_and_slide()
+
+func movement(delta):
+	var input_direction = Vector2(Input.get_axis("Move Left", "Move Right"), Input.get_axis("Move Up", "Move Down"))
+	var lerp_time = delta * (acceleration if input_direction else friction)
+	
+	velocity = lerp(velocity, input_direction.normalized() * current_max_speed, lerp_time)
+	animate(input_direction)
+
+func animate(input_direction):
+	if input_direction.x == 1.0:
+		$AnimatedSprite2D.flip_h = true
+	elif input_direction.x == -1:
+		$AnimatedSprite2D.flip_h = false
+	if input_direction == Vector2.ZERO:
+		$AnimatedSprite2D.play("idle")
+	else:
+		$AnimatedSprite2D.play("run")
